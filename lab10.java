687a00316e7eadd675c7a0dd2c1a068b7aa062ef
katta-hema104 <71824954+katta-hema104@users.noreply.github.com>
2020-12-02 12:08:56 +0530

    Add files via upload

--- lab10.java
+++ lab10.java
@@ -1,21 +1,20 @@
-import java.util.*;
 class lab10
 {
  static int count=0;
- static void lab10()
+ static void incr()
  {
   count++;
  }
  public static void main(String args[])
  {
  lab10 c1=new lab10();
- c1.lab10();
+ c1.incr();
  lab10 c2=new lab10();
- c2.lab10();
+ c2.incr();
  lab10 c3=new lab10();
- c3.lab10();
+ c3.incr();
  lab10 c4=new lab10();
- c4.lab10();
+ c4.incr();
  System.out.println("number of objects="+count);
-}
+ }
 }
